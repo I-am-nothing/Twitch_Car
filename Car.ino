7150a7af34a354c78a24dfe3e0bd5d4483363041
conniehaaaaa <91178159+conniehaaaaa@users.noreply.github.com>
2021-09-29 15:26:07 +0800

    Update Car.ino

--- Car.ino
+++ Car.ino
@@ -1,6 +1,14 @@
-#include "Motor.h"
+//#include "Motor.h"
+#include <WiFi.h>
+//Motor motor;
+WiFiClient client;
 
-Motor motor;
+const char* ssid = "小騷包魚有C. 菌";
+const char* password =  "sakun921118";
+ 
+const uint16_t port = 9700;
+const char * host = "192.168.240.187";
+ 
 
 int motorNow[4][3] = {
   {0, 1, 1024},
@@ -9,12 +17,55 @@ int motorNow[4][3] = {
   {0, 0, 1024}
 };
 
+void socketConnect(){
+  while (!client.connect(host, port)) {
+    Serial.println("Connection to host failed");
+    delay(1000);
+  }
+}
+
 void setup() {
+
+ Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.print("/nWiFi connected with IP: ");
+  Serial.println(WiFi.localIP()); 
   
+  socketConnect();
 }
 
 void loop() {
-  for(int i=-1024; i<1024; i+=5){
+  
+  if(client.connected()){
+    client.println("AAA BBB CCC");
+    while (client.available()){
+      String line = client.readStringUntil('\n'); 
+      Serial.print("recieve data => "); 
+      Serial.println(line); 
+      client.write(line.c_str()); 
+    }
+  }
+  else{
+    socketConnect();
+  }
+  delay(1);
+ 
+    //Serial.println("Disconnecting...");
+    //client.stop();
+ 
+    //delay(10000);
+
+   
+
+
+  
+  /*for(int i=-1024; i<1024; i+=5){
     motorNow[0][2] = abs(i);
     if(i == 1){
       motorNow[0][0] = !motorNow[0][0];
@@ -22,5 +73,5 @@ void loop() {
     }
     motor.setMotor(motorNow);
     delay(10);
-  }
+  }*/
 }
